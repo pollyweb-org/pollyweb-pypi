06006fdf51388f67918226980de71b95e31d68e3
Jorge Fonseca <jorgemjfonseca@users.noreply.github.com>
2026-02-23 23:40:20 +0000

    .

---
+++ README.md
@@ -0,0 +1,10 @@
+# pollyweb
+
+A sample Python package for PyPI.
+
+## Usage
+
+```
+from pollyweb import hello
+print(hello())
+```
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pollyweb",
+    version="0.1.0",
+    description="A sample Python package for PyPI.",
+    author="jorgemf",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.7',
+)
